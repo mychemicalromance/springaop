57f76a097e87a6400fded40ec76fa7cac0a99c0f
xukeqiong02 <xukeqiong02@meituan.com>
2023-06-09 15:44:07 +0800

    a

--- src/main/java/com/example/springaop/SpringaopApplication.java
+++ src/main/java/com/example/springaop/SpringaopApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringaopApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringaopApplication.class, args);
 	}
-
 }
